dc9d522f0cfc742b4b6289de23c49d508e667839
Milena Karpenko <milenness06@gmail.com>
2026-08-21 19:38:32 +0300

    add/mobile menu

--- components/MobileMenu/MobileMenu.tsx
+++ components/MobileMenu/MobileMenu.tsx
@@ -1,11 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import css from "./MobileMenu.module.css";
+import { VscCloseCompact } from "react-icons/vsc";
+
 
 interface ModalProps {
   onClose: () => void;
 }
 
-export default function Modal({ onClose }: ModalProps) {
+export default function MobileMenu({ onClose }: ModalProps) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
   return createPortal(
     <div className={css.backdrop}>
       <div className={css.modal}>
@@ -14,11 +30,61 @@ export default function Modal({ onClose }: ModalProps) {
           onClick={onClose}
           aria-label="Close modal"
         >
-          &times;
+          <VscCloseCompact size={18} />
         </button>
 
-        <h2>Modal Title</h2>
-        <p>This is some content inside the modal.</p>
+        <ul className={css.navList}>
+          <li className={css.navItem}>
+            <Link
+              href="/"
+              className={`${css.navLink} ${pathname === "/" ? css.active : ""}`}
+              style={{ paddingLeft: "20px", paddingRight: "20px" }}
+              aria-label="Go to home page"
+              onClick={onClose}
+            >
+              Home
+            </Link>
+          </li>
+          <li className={css.navItem}>
+            <Link
+              href="/medicine-store"
+              className={`${css.navLink} ${
+                pathname === "/medicine-store" ? css.active : ""
+              }`}
+              style={{ paddingLeft: "9px", paddingRight: "9px" }}
+              aria-label="Go to medicine stores page"
+              onClick={onClose}
+            >
+              Medicine store
+            </Link>
+          </li>
+          <li className={css.navItem}>
+            <Link
+              href="/medicine"
+              className={`${css.navLink} ${
+                pathname === "/medicine" ? css.active : ""
+              }`}
+              style={{ paddingLeft: "17px", paddingRight: "17px" }}
+              aria-label="Go to medicine catalog page"
+              onClick={onClose}
+            >
+              Medicine
+            </Link>
+          </li>
+        </ul>
+
+        <ul className={css.authList}>
+          <li className={css.authItem}>
+            <Link href="/register" className={css.registerLink}>
+              Register
+            </Link>
+          </li>
+          <li className={css.authItem}>
+            <Link href="/login" className={css.loginLink}>
+              Login
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>,
     document.body,
