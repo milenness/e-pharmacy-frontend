49b13e38e620bec022fe0065a343431992c637c3
Milena Karpenko <milenness06@gmail.com>
2026-08-25 21:20:32 +0300

    add medicine page

--- app/(main)/(private routes)/medicine/page.tsx
+++ app/(main)/(private routes)/medicine/page.tsx
@@ -1,9 +1,13 @@
+import css from "./page.module.css"
+
 export default function MedicinePage() {
   return (
-    <div>
-      <h1>Medicine</h1>
-      {/* <SearchFilterPanel />
-      <ProductCatalog />  */}
-    </div>
+    <section className={css.section}>
+     <div className="container">
+        <h1>Medicine</h1>
+        {/* <SearchFilterPanel />
+        <ProductCatalog />  */}
+     </div>
+    </section>
   );
 }
