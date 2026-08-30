e7840f421f76dfe282c437aa1bc0c4aa297424fb
Milena Karpenko <milenness06@gmail.com>
2026-08-30 18:02:53 +0300

    add desk component

--- components/DetailInfo/DetailInfo.tsx
+++ components/DetailInfo/DetailInfo.tsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { useState } from "react";
 import css from "./DetailInfo.module.css";
 import Image from "next/image";
 import { CgMathPlus } from "react-icons/cg";
 import { HiMiniMinus } from "react-icons/hi2";
 
 export default function DetailInfo() {
+  const [activeTab, setActiveTab] = useState<"description" | "reviews">(
+    "description",
+  );
+
   return (
     <div className={css.detailWrapper}>
       <Image
@@ -35,60 +42,89 @@ export default function DetailInfo() {
           </button>
         </div>
       </div>
+
       <div className={css.deskWrapper}>
         <ul className={css.buttonsWrapper}>
           <li>
-            <button type="button">Description</button>
+            <button
+              type="button"
+              className={`${css.deskButton} ${activeTab === "description" ? css.active : ""}`}
+              onClick={() => setActiveTab("description")}
+            >
+              Description
+            </button>
           </li>
           <li>
-            <button type="button">Reviews</button>
+            <button
+              type="button"
+              className={`${css.deskButton} ${activeTab === "reviews" ? css.active : ""}`}
+              onClick={() => setActiveTab("reviews")}
+            >
+              Reviews
+            </button>
           </li>
         </ul>
-        <p className={css.mainDesk}>
-          Although it&apos;s typically considered safe, excessive consumption
-          can lead to side effects. Therefore, it&apos;s recommended to consult
-          a healthcare professional before using moringa, especially if
-          you&apos;re pregnant, nursing, or taking other medications. This
-          balanced approach allows for the benefits of moringa while recognizing
-          the importance of proper usage and caution.
-        </p>
-        <ul>
-          <li>
-            <span className={css.accent}>Medicinal Uses:</span>
-            <span className={css.accent}> Antioxidant Properties:</span> Moringa
-            is packed with antioxidants that help fight oxidative stress and
-            inflammation in the body.
-          </li>
-          <li>
-            <span className={css.accent}>Anti-Diabetic Effects:</span> Some
-            studies have shown that moringa leaves might lower blood sugar
-            levels, making it a valuable supplement for managing diabetes.
-          </li>
-          <li>
-            <span className={css.accent}>Anti-Diabetic Effects:</span> Some
-            studies have shown that moringa leaves might lower blood sugar
-            levels, making it a valuable supplement for managing diabetes.
-          </li>
-          <li>
-            <span className={css.accent}>Heart Health:</span> The plant has been
-            linked to reduced cholesterol levels, which is vital for heart
-            health.
-          </li>
-          <li>
-            <span className={css.accent}>Anti-Cancer Properties:</span> Certain
-            compounds in moringa, such as niazimicin, have been found to
-            suppress the growth of cancer cells in laboratory studies.
-          </li>
-          <li>
-            <span className={css.accent}>Immune Support:</span> With its high
-            vitamin C content, moringa can boost the immune system.
-          </li>
-          <li>
-            <span className={css.accent}>Digestive Aid:</span> Moringa can help
-            in treating digestive disorders due to its anti-inflammatory
-            properties.
-          </li>
-        </ul>
+
+        {activeTab === "description" && (
+          <>
+            <p className={css.mainDesk}>
+              Although it&apos;s typically considered safe, excessive
+              consumption can lead to side effects. Therefore, it&apos;s
+              recommended to consult a healthcare professional before using
+              moringa, especially if you&apos;re pregnant, nursing, or taking
+              other medications. This balanced approach allows for the benefits
+              of moringa while recognizing the importance of proper usage and
+              caution.
+            </p>
+            <ul className={css.deskList}>
+              <li className={css.deskItem}>
+                <span className={css.accent}>Medicinal Uses:</span>
+                <span className={css.accent}>
+                  {" "}
+                  Antioxidant Properties:
+                </span>{" "}
+                Moringa is packed with antioxidants that help fight oxidative
+                stress and inflammation in the body.
+              </li>
+              <li className={css.deskItem}>
+                <span className={css.accent}>Anti-Diabetic Effects:</span> Some
+                studies have shown that moringa leaves might lower blood sugar
+                levels, making it a valuable supplement for managing diabetes.
+              </li>
+              <li className={css.deskItem}>
+                <span className={css.accent}>Anti-Diabetic Effects:</span> Some
+                studies have shown that moringa leaves might lower blood sugar
+                levels, making it a valuable supplement for managing diabetes.
+              </li>
+              <li className={css.deskItem}>
+                <span className={css.accent}>Heart Health:</span> The plant has
+                been linked to reduced cholesterol levels, which is vital for
+                heart health.
+              </li>
+              <li className={css.deskItem}>
+                <span className={css.accent}>Anti-Cancer Properties:</span>{" "}
+                Certain compounds in moringa, such as niazimicin, have been
+                found to suppress the growth of cancer cells in laboratory
+                studies.
+              </li>
+              <li className={css.deskItem}>
+                <span className={css.accent}>Immune Support:</span> With its
+                high vitamin C content, moringa can boost the immune system.
+              </li>
+              <li className={css.deskItem}>
+                <span className={css.accent}>Digestive Aid:</span> Moringa can
+                help in treating digestive disorders due to its
+                anti-inflammatory properties.
+              </li>
+            </ul>
+          </>
+        )}
+
+        {activeTab === "reviews" && (
+          <ul className={css.reviewsList}>
+           <li></li>
+          </ul>
+        )}
       </div>
     </div>
   );
