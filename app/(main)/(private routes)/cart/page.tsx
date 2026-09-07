ffa89e4b152ad1a4fbfe56d1ba6dc512af43e295
Milena Karpenko <milenness06@gmail.com>
2026-09-07 11:56:09 +0300

    add cart page

--- app/(main)/(private routes)/cart/page.tsx
+++ app/(main)/(private routes)/cart/page.tsx
@@ -1,10 +1,15 @@
+import css from "./page.module.css";
+
 export default function CartPage() {
   return (
-    <div>
-      {/* <ShippingInfo />
-      <PaymentMethod />
-      <CartList /> 
-      <OrderDetails />  */}
-    </div>
+    <section className={css.section}>
+      <div className="container">
+        <h2 className={css.title}>Cart</h2>
+        {/* <ShippingInfo />
+        <PaymentMethod />
+        <OrderDetails />  */}
+        {/* <CartList /> */}
+      </div>
+    </section>
   );
 }
